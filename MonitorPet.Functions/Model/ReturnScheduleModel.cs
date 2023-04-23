ff053927994f5f91ea6eb74b94592c5eb5cfae86
GuilhermeBley <guilhermecbley@gmail.com>
2023-04-23 10:59:03 -0300

    Add field LastRelease #12

---
+++ MonitorPet.Functions/Model/ReturnScheduleModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MonitorPet.Functions.Model;
+
+internal class ReturnScheduleModel
+{
+    public DateTime? LastRelease { get; set; } = null;
+    public IEnumerable<ScheduleModel> Schedules { get; set; }
+        = Enumerable.Empty<ScheduleModel>();
+}
